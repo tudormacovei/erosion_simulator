0adac9800bf2f30ba21558f07a0019008073df17
Macovei Tudor <tudor_macovei@iCloud.com>
2025-02-25 17:42:53 +0100

    Add rudimentary erosion functionality

--- erosion_simulator.cpp
+++ erosion_simulator.cpp
@@ -4,12 +4,169 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <chrono>
+#include <random>
 
 #include "lodepng.h"
+
+#define RNG_MARGINS 10		// the number of pixels the droplet placement should be distanced from the edges of the image, at minimum
+
+#define ITERATIONS 10000
+#define EVAPORATION 0.002f
+#define INTENSITY 4
+#define S_DR 0.01f
+#define S_DF 0.0005f
+#define S_TF 0.0001f
+#define S_TR 0.01f
+#define STARTING_WATER 1.0f
 
 char int_to_ascii(unsigned char &value) {
 	const char lightness_map[66] = "`^\",:;Il!i~+_-?][}{1)(|\\/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8%B@$";
 	return lightness_map[value / 4];
+}
+
+// Gets the tanget at the given point, with padding at the edges by copying the point's height
+std::pair<float, float> get_tangent(float** heights, unsigned int width, unsigned int height, std::pair<unsigned int, unsigned int> point)
+{
+	float bottom = point.first < height - 1 ? heights[point.first + 1][point.second] : heights[point.first][point.second];
+	float right = point.second < width - 1 ? heights[point.first][point.second + 1] : heights[point.first][point.second];
+	float left = point.second > 0 ? heights[point.first][point.second - 1] : heights[point.first][point.second];
+	float top = point.first > 0 ? heights[point.first - 1][point.second] : heights[point.first][point.second];
+
+	return std::make_pair((bottom - top) / 2.0f, (right - left) / 2.0f);
+}
+
+// Performs one erosion step by simulating the erosion of one 'droplet'
+// Modifies: float** heights
+void erosion_step(float** heights, unsigned int width, unsigned int height, std::pair<unsigned int, unsigned int> point)
+{
+	float water_amount = STARTING_WATER;
+	float carried_soil = 0.0f;
+	float velocity = 0.0f;
+	std::pair<float, float> direction {0.0f, 0.0f};
+	
+	std::uniform_real_distribution random_float(-1.0f, 1.0f);
+	std::mt19937 gen(0);
+
+	auto next_point = point;
+	while (water_amount > 0.0f)
+	{
+		// get the tangent at the current point
+		direction = get_tangent(heights, width, height, point);
+
+		// if tangent is close to 0, choose random direction
+		if (std::abs(direction.first) <= 0.1f || std::abs(direction.second) <= 0.1f)
+		{
+			direction.first = random_float(gen);
+			direction.second = random_float(gen);
+		}
+
+		float slope;
+		// based on direction, choose next point and compute slope
+		// TODO: HANDLE 'pits'
+		if (std::abs(direction.first) > std::abs(direction.second))
+		{
+			if (direction.first > 0.0f) // the slope is pointing to the north
+			{
+				next_point.first -= 1;
+			}
+			else
+			{
+				next_point.first += 1;
+			}
+			slope = std::abs(direction.first);
+		}
+		else
+		{
+			if (direction.second > 0.0f) // the slope is pointing to the west
+			{
+				next_point.second -= 1;
+			}
+			else
+			{
+				next_point.second += 1;
+			}
+			slope = std::abs(direction.second);
+		}
+		if (next_point.first < 0 || next_point.first >= height || next_point.second < 0 || next_point.second >= width)
+		{
+			return; // the droplet has left the simulation bounds
+		}
+
+		// perform erosion or deposition
+		float d_r = S_DR * std::pow(INTENSITY, 2.0f);
+		float d_f = S_DF * std::pow(slope, 2.0f / 3.0f) * std::pow(velocity, 2.0f / 3.0f);
+		float t_r = S_TR * slope * INTENSITY;
+		float t_f = S_TF * std::pow(slope, 5.0f / 3.0f) * std::pow(velocity, 5.0f / 3.0f);
+		
+		float detached_soil = d_r + d_f;
+		float transport_capacity = t_r + t_f;
+		
+		carried_soil += detached_soil;
+
+		// it does not make sense for the next point to be at a higher position than our current point
+		if (heights[next_point.first][next_point.second] > heights[point.first][point.second])
+		{
+			auto diff = heights[next_point.first][next_point.second] - heights[point.first][point.second];
+			auto deposited = std::min(diff, carried_soil);
+			carried_soil -= deposited;
+			velocity = 0.0f;
+			// we do not update the point location, it could be permanently stuck
+		}
+		else
+		{
+			float sedimented_soil = std::max(carried_soil - transport_capacity, 0.0f);
+			float eroded_soil = detached_soil - sedimented_soil;
+
+			if (sedimented_soil > 0.0f)
+			{
+				heights[point.first][point.second] += sedimented_soil;
+			}
+			else
+			{
+				heights[point.first][point.second] -= eroded_soil;
+			}
+			point = next_point;
+		}
+
+		// heights[point.first][point.second] = 255.0f; // TO MARK
+		water_amount -= EVAPORATION;
+	}
+}
+
+void erode_image(unsigned char** pixels, unsigned int width, unsigned int height)
+{
+	float** heights = new float* [height];
+	for (int i = 0; i < height; ++i) {
+		heights[i] = new float[width];
+	}
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			heights[i][j] = (float) pixels[i][j];
+		}
+	}
+
+	std::random_device rd;
+	std::mt19937 gen(0);	// set a constant seed for testing purposes
+	std::uniform_int_distribution<unsigned> distrib_width(RNG_MARGINS, width - RNG_MARGINS);
+	std::uniform_int_distribution<unsigned> distrib_height(RNG_MARGINS, height - RNG_MARGINS);
+
+	for (int i = 0; i < ITERATIONS; i++)
+	{
+		erosion_step(heights, width, height, std::make_pair(distrib_width(gen), distrib_height(gen)));
+	}
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < width; j++)
+		{
+			heights[i][j] = std::clamp(heights[i][j], 0.0f, 255.0f); // to ensure type conversion safety
+			pixels[i][j] = (unsigned char)heights[i][j];
+		}
+	}
 }
 
 int main()
@@ -27,17 +184,17 @@ int main()
 	}
 
 	// Create a 2D array to store pixel values
-	unsigned char** pixelValues = new unsigned char* [height];
+	unsigned char** pixels = new unsigned char* [height];
 	for (int i = 0; i < height; ++i) {
-		pixelValues[i] = new unsigned char[width];
+		pixels[i] = new unsigned char[width];
 	}
 
 	// Copy pixel values to the array
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
 			// 4 bytes per pixel (RGBA), we poll the R byte - and assume a greyscale image
-			pixelValues[i][j] = image[(i * height + j) * 4];
-			std::cout << int_to_ascii(pixelValues[i][j]) << ' ';
+			pixels[i][j] = image[(i * height + j) * 4];
+			//std::cout << int_to_ascii(pixels[i][j]) << ' ';
 			
 			// clear image to ensure that the writing is without any interference from the read
 			image[(i * height + j) * 4] = 0;
@@ -45,16 +202,18 @@ int main()
 			image[(i * height + j) * 4 + 2] = 0;
 			image[(i * height + j) * 4 + 3] = 0;
 		}
-		std::cout << '\n';
+		//std::cout << '\n';
 	}
+
+	erode_image(pixels, width, height);
 
 	// write pixel values to PNG vector (RGBA) bytes
 	for (int i = 0; i < height; ++i) {
 		for (int j = 0; j < width; ++j) {
 			// 4 bytes per pixel (RGBA), we poll the R byte - and assume a greyscale image
-			image[(i * height + j) * 4] = pixelValues[i][j];
-			image[(i * height + j) * 4 + 1] = pixelValues[i][j];
-			image[(i * height + j) * 4 + 2] = pixelValues[i][j];
+			image[(i * height + j) * 4] = pixels[i][j];
+			image[(i * height + j) * 4 + 1] = pixels[i][j];
+			image[(i * height + j) * 4 + 2] = pixels[i][j];
 			image[(i * height + j) * 4 + 3] = 255; // Alpha byte
 		}
 	}
